91a5a4d64fb542de452f79553769b64b5e939dae
rabi1924 <rabi2024@gmail.com>
2022-05-04 11:50:41 +0900

    sorcery導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root "top_pages#show"
+
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end

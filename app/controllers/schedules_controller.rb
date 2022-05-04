3faa8b3a5faa056eec9abc56ddc2e8795b7e1c24
KouSei089 <qq00891829@icloud.com>
2022-05-04 10:17:42 +0900

    Add:scheduleの新規作成機能を作成しました

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,7 +3,24 @@ class SchedulesController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    schedule = Schedule.new(schedule_params)
+    if schedule.save
+      redirect_to schedules_path, notice: 'Schedule was successfully created.'
+    else
+      render :new
+    end
+  end
 
   def edit; end
+
+  private
+
+    def schedule_params
+      params.require(:schedule).permit(:title)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "top_pages#show"
+
+  resources :schedules
 end

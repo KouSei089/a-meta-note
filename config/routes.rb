29b2c6fedcc9bf3abcf2a2a8c3c609d10e97a1b8
KouSei089 <qq00891829@icloud.com>
2022-05-04 23:13:45 +0900

    Fix:sorcery導入のissueのバグと改善点を修正しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "top_pages#show"
   resources :schedules
-  resources :users
+  resources :users, only: [:new, :create]
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   post 'logout' => 'user_sessions#destroy', :as => :logout

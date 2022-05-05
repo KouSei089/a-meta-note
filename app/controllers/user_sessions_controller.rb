dfe05b0990a5516f67dd00c6c07162034c2745e0
KouSei089 <qq00891829@icloud.com>
2022-05-05 09:45:05 +0900

    Fix:パスがrootだったのでschedule indexページに遷移するように修正しました

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to root_path, notice: 'Login successful'
+      redirect_back_or_to schedules_path, notice: 'Login successful'
     else
       flash.now[:alert] = 'Login failed'
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to root_path, notice: "User was successfully created."
+      redirect_to schedules_path, notice: "User was successfully created."
     else
       render :new, status: :unprocessable_entity
     end

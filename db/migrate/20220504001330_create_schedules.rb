fe4d9e3177e965f6fa039bf7e295c9a32be2de51
rabi1924 <rabi2024@gmail.com>
2022-05-05 10:43:03 +0900

    change:schedule title NOT NULL制約

--- db/migrate/20220504001330_create_schedules.rb
+++ db/migrate/20220504001330_create_schedules.rb
@@ -1,7 +1,7 @@
 class CreateSchedules < ActiveRecord::Migration[7.0]
   def change
     create_table :schedules do |t|
-      t.date :title
+      t.date :title, null: false
       t.float :analysis
 
       t.timestamps

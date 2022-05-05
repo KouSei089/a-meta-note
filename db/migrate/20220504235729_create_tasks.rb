82c976dc96292c468434c7eb0cc40eec8b9ec38f
rabi1924 <rabi2024@gmail.com>
2022-05-05 10:40:09 +0900

    change：taskテーブルの名称変更をする

--- db/migrate/20220504235729_create_tasks.rb
+++ db/migrate/20220504235729_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.string  :task_title, null: false
-      t.integer :task_category_id, null: false
-      t.time    :task_resource, null: false
+      t.string  :title, null: false
+      t.integer :category_id, null: false
+      t.time    :resource, null: false
       t.timestamps
     end
   end

25831f18170ad79800731c2b48ea641af8ccfbe4
rabi1924 <rabi2024@gmail.com>
2022-05-06 00:51:51 +0900

    Revert "Add:scheduleフォームにcategoryのリンク作成"
    This reverts commit 3a86e2292ad689075ace5e736d6e2472c9d0deda.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,45 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:edit, :update, :destroy]
+   def new; end
 
-  def index
-    @category = Category.new
-    @categories = Category.all
-  end
-
-  def new; end
-
-  def create
-    @category = Category.new(category_params)
-    if @category.save
-      redirect_to categories_path
-    else
-      @categories = Category.all
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @category.update(category_params)
-      redirect_to categories_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @category.destroy
-    redirect_to categories_path
-  end
-
-  private
-
-  def set_category
-    @category = Category.find(params[:id])
-  end
-
-  def category_params
-    params.require(:category).permit(:name).merge(user_id: current_user.id)
-  end
+   def create; end
 end

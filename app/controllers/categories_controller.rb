9ed69fdc479923b0363f9a3c909866e9d079cef9
rabi1924 <rabi2024@gmail.com>
2022-05-06 09:50:26 +0900

    Fix:categoryのコントローラー修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,15 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:edit, :update, :destroy]
   def index
-    @category = Category.new
     @categories = Category.all
   end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
 
   def create
-    @category = Category.new(category_params)
+    @category = current_user.categories.build(category_params)
     if @category.save
       redirect_to categories_path
     else
@@ -35,11 +36,10 @@ class CategoriesController < ApplicationController
   private
 
   def set_category
-    @category = Category.find(params[:id])
+    @category = current_user.categories.find(params[:id])
   end
 
   def category_params
-    params.require(:category).permit(:name).merge(user_id: current_user.id)
+    params.require(:category).permit(:name)
   end
-   def create; end
 end

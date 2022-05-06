dc22a8f3ba5f47ef7f806864903cfb4afd44407c
rabi1924 <rabi2024@gmail.com>
2022-05-06 09:47:15 +0900

    Fix:userモデルのタイポ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   has_many :schedules, dependent: :destroy
-  has_many :category, dependent: :destroy
+  has_many :categories, dependent: :destroy
 
   validates :username, presence: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }

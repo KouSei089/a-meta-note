3b08dc85e90badcd6d44446ba90ae7ea9c007367
rabi1924 <rabi2024@gmail.com>
2022-05-07 11:35:18 +0900

    Update:categoryのcolorのカラム変更と追加

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_05_002928) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.string "color"
+    t.integer "color_id"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

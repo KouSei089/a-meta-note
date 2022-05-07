b2fde5b9444659fb335423b1776ec63b56ec6fcf
rabi1924 <rabi2024@gmail.com>
2022-05-07 10:05:43 +0900

    Add:ActiveHashモデル作成

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_many :colors
 
   serialize :name, Array
   serialize :color, Array

adeb628d52c9f562c71bde15a2e5b574a41853a4
KouSei089 <qq00891829@icloud.com>
2022-05-08 15:15:43 +0900

    Update:Colorモデルにcolor_numカラムの追加を行いました

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,10 +1,10 @@
 class Color < ActiveHash::Base
   self.data = [
-    { id: 1, name: '青' },
-    { id: 2, name: '赤' },
-    { id: 3, name: '橙' },
-    { id: 4, name: '黄' },
-    { id: 5, name: '緑' },
-    { id: 6, name: '紫' }
+    { id: 1, name: 'blue', color_num: '#0367A6' },
+    { id: 2, name: 'red', color_num: '#D93232' },
+    { id: 3, name: 'orange', color_num: '#F27F3D' },
+    { id: 4, name: 'yellow', color_num: '#F2CA50' },
+    { id: 5, name: 'green', color_num: '#08A63F' },
+    { id: 6, name: 'purple', color_num: '#886FBF' }
   ]
 end

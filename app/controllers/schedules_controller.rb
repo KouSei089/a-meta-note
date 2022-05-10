f9cd088c44a87d02e8a195ed7083e0f9126336c5
KouSei089 <qq00891829@icloud.com>
2022-05-10 13:42:48 +0900

    Add:colorの分析結果の反映かつカテゴリーが重複したとき集計するロジックを実装しました

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -10,12 +10,21 @@ class SchedulesController < ApplicationController
     @task = current_user.tasks.new
     @category_array = []
     @percent_array = []
+    @color_array = []
     @tasks.each do |t|
-      @category_array.push(t.category_name)
-      @percent_array.push(t.percent)
+      if @category_array.include?(t.category_name)
+        index_num = @category_array.index(t.category_name)
+        @percent_array.insert(index_num, (@percent_array[index_num] + t.percent))
+        @percent_array.delete_at(index_num + 1)
+      else
+        @category_array.push(t.category_name)
+        @percent_array.push(t.percent)
+        @color_array.push(t.task_categories[0].category.color_code)
+      end
     end
     gon.category_array = @category_array
     gon.percent_array = @percent_array
+    gon.color_array = @color_array
   end
 
   def new

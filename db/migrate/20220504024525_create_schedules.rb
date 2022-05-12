cb3625e0c55b77c54bc53ab2d13eb00e6538e5e3
KouSei089 <qq00891829@icloud.com>
2022-05-12 22:26:17 +0900

    Remove:scheduleテーブルの不要なカラムを削除しました

--- db/migrate/20220504024525_create_schedules.rb
+++ db/migrate/20220504024525_create_schedules.rb
@@ -2,7 +2,6 @@ class CreateSchedules < ActiveRecord::Migration[7.0]
   def change
     create_table :schedules do |t|
       t.date :title, null: false
-      t.integer :analysis
       t.references :user, null: false, foreign_key: true
 
       t.timestamps

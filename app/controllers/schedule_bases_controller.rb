4e12dd916d3fe5d2561e448cbc208910bbd495fb
rabi1924 <rabi2024@gmail.com>
2022-05-16 15:23:18 +0900

    Add:新規作成機能のターボ連動と値を追加できるようにしました

--- app/controllers/schedule_bases_controller.rb
+++ app/controllers/schedule_bases_controller.rb
@@ -17,6 +17,27 @@ class ScheduleBasesController < ApplicationController
     @schedule_basis = current_user.schedule_bases.find(params[:id])
     @task_basis = current_user.task_bases.new
     @task_bases = @schedule_basis.task_bases.all
+    @category_array = []
+    @percent_array = []
+    @color_array = []
+    @task_bases.each do |t|
+      if @category_array.include?(t.category_name)
+        index_num = @category_array.index(t.category_name)
+        @percent_array.insert(index_num, (@percent_array[index_num] + t.percent))
+        @percent_array.delete_at(index_num + 1)
+      else
+        @category_array.push(t.category_name)
+        @percent_array.push(t.percent)
+        if @category_array.include?("Untitled")
+          @color_array.push("rgba(166,166,166,0.5)")
+        else
+          @color_array.push(t.category_task_bases[0].category.color_code)
+        end
+      end
+    end
+    gon.category_array = @category_array
+    gon.percent_array = @percent_array
+    gon.color_array = @color_array
   end
 
   private

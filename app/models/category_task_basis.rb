d8f7fcf4b6d3be112af31e2b83e740fd69cab058
rabi1924 <rabi2024@gmail.com>
2022-05-16 10:06:16 +0900

    Add:schedule_basis, task_basis, category_task basisのDB設計

---
+++ app/models/category_task_basis.rb
@@ -0,0 +1,2 @@
+class CategoryTaskBasis < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_15_070723) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_16_003409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_15_070723) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "category_task_bases", force: :cascade do |t|
+    t.bigint "task_basis_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_task_bases_on_category_id"
+    t.index ["task_basis_id"], name: "index_category_task_bases_on_task_basis_id"
   end
 
   create_table "schedule_bases", force: :cascade do |t|
@@ -38,6 +47,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_15_070723) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_schedules_on_user_id"
+  end
+
+  create_table "task_bases", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "category_name", null: false
+    t.time "time_start", null: false
+    t.time "time_end", null: false
+    t.integer "percent"
+    t.bigint "schedule_basis_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["schedule_basis_id"], name: "index_task_bases_on_schedule_basis_id"
+    t.index ["user_id"], name: "index_task_bases_on_user_id"
   end
 
   create_table "task_categories", force: :cascade do |t|
@@ -74,8 +97,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_15_070723) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "category_task_bases", "categories"
+  add_foreign_key "category_task_bases", "task_bases"
   add_foreign_key "schedule_bases", "users"
   add_foreign_key "schedules", "users"
+  add_foreign_key "task_bases", "schedule_bases"
+  add_foreign_key "task_bases", "users"
   add_foreign_key "task_categories", "categories"
   add_foreign_key "task_categories", "tasks"
   add_foreign_key "tasks", "schedules"

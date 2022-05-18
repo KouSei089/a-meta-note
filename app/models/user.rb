f8f3afeb217b211746429ea8132e80c3d55f68e9
KouSei089 <qq00891829@icloud.com>
2022-05-18 16:18:35 +0900

    Add:Userのバリデーションを設定し、それに伴うメッセージをフォームに記述しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,12 @@ class User < ApplicationRecord
   has_many :task_bases, dependent: :destroy
   has_many :categories, dependent: :destroy
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)(?=.*?[!-~&&[^a-z\d]])[!-~]{8,100}+\z/i.freeze
   validates :username, presence: true
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: 'は8文字以上にしてください。英数字の両方を含む必要があります' }, if: -> {
+                                                                                                                                new_record? || changes[:crypted_password]
+                                                                                                                              }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-
-  validates :email, uniqueness: true
 end

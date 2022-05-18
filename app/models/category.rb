51fe629fab67110b75ec29c04772676dffbaeaf5
KouSei089 <qq00891829@icloud.com>
2022-05-18 16:26:58 +0900

    Add:categoryのバリデーションとNOT NULL制約を設定しました

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,8 @@ class Category < ApplicationRecord
   has_many :tasks, through: :task_categories
   has_many :category_task_bases, dependent: :destroy
   has_many :task_bases, through: :category_task_bases
+
+  validates :name, presence: true
+  validates :color, presence: true
+  validates :color_code, presence: true
 end
--- db/migrate/20220505002928_create_categories.rb
+++ db/migrate/20220505002928_create_categories.rb
@@ -1,9 +1,9 @@
 class CreateCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :categories do |t|
-      t.string :name
-      t.string :color
-      t.string :color_code
+      t.string :name, null: false
+      t.string :color, null: false
+      t.string :color_code, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end

e467cd85fb3ae44d92e97511306ea0946cec836e
KouSei089 <qq00891829@icloud.com>
2022-05-19 14:16:14 +0900

    Add:gem meta_tagsを導入し、ファビコンやその他SEO関連の設定ファイルを作成しました

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'a meta note',
+      title: 'a meta note',
+      reverse: false,
+      charset: 'utf-8',
+      description: 'Analyze your daily schedule get the optimal lifestyle.',
+      keywords: 'schedule, note, analysis',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.png') },
+        { href: image_url('twitter_header_photo_2.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' }
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('logo.png'),
+        locale: 'ja_JP'
+      }
+      # twitter: {
+      #   card: 'summary',
+      #   site: '@ツイッターのアカウント名',
+      # }
+    }
+  end
 end

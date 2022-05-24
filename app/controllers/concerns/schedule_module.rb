74f9c3f5aa6fb4f3997dbb244ce49a9d7b0eabfc
KouSei089 <qq00891829@icloud.com>
2022-05-24 23:26:17 +0900

    Clean:scheduleコントローラのロジックをモジュール化しました

--- app/controllers/concerns/schedule_module.rb
+++ app/controllers/concerns/schedule_module.rb
@@ -1,6 +1,24 @@
 module ScheduleModule
   extend ActiveSupport::Concern
 
+  def add_task_basis_analyses(task_bases)
+    task_bases.each do |t|
+      if @basis_category_array.include?(t.category_name)
+        index_num = @basis_category_array.index(t.category_name)
+        @basis_percent_array.insert(index_num, (@basis_percent_array[index_num] + t.percent))
+        @basis_percent_array.delete_at(index_num + 1)
+      else
+        @basis_category_array.push(t.category_name)
+        @basis_percent_array.push(t.percent)
+        if @basis_category_array.include?("Untitled")
+          @basis_color_array.push("rgba(166,166,166,0.5)")
+        else
+          @basis_color_array.push(t.category_task_bases[0].category.color_code)
+        end
+      end
+    end
+  end
+
   def add_task_analyses(tasks)
     tasks.each do |t|
       if @category_array.include?(t.category_name)
@@ -13,9 +31,43 @@ module ScheduleModule
         if @category_array.include?("Untitled")
           @color_array.push("rgba(166,166,166,0.5)")
         else
-          @color_array.push(t.category_task_bases[0].category.color_code)
+          @color_array.push(t.task_categories[0].category.color_code)
         end
       end
+    end
+  end
+
+  def pattren_select(schedule)
+    if schedule.pattren.include?("weekday")
+      current_user.schedule_bases.find_by(pattren: "weekday")
+    else
+      current_user.schedule_bases.find_by(pattren: "holiday")
+    end
+  end
+
+  def analysis_feedback(tasks, task_bases)
+    @name_results = []
+    @color_results = []
+    @percent_results = []
+    task_category_results = []
+    task_category_names = tasks.pluck(:category_name).uniq
+    task_basis_category_names = task_bases.pluck(:category_name).uniq
+    task_category_results = task_category_names & task_basis_category_names
+    index_num = 0
+    task_category_results.count.times do
+      task_categories_sames = tasks.where(category_name: task_category_results[index_num])
+      task_percent_sum = task_categories_sames.pluck(:percent).sum
+      task_basis_categories_sames = task_bases.where(category_name: task_category_results[index_num])
+      task_basis_percent_sum = task_basis_categories_sames.pluck(:percent).sum
+      percent_result = if task_percent_sum > task_basis_percent_sum
+                         task_percent_sum - task_basis_percent_sum
+                       else
+                         task_basis_percent_sum - task_percent_sum
+                       end
+      @name_results.push(task_categories_sames[0].category_name)
+      @color_results.push(task_categories_sames[0].categories[0].color_code)
+      @percent_results.push(percent_result)
+      index_num += 1
     end
   end
 end
--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,6 +2,8 @@ class SchedulesController < ApplicationController
   before_action :set_schedule, only: [:show, :destroy]
   before_action :set_q, only: [:index]
 
+  include ScheduleModule
+
   def index
     @schedule = Schedule.new
     @results = @q.result.order(title: :DESC)
@@ -13,82 +15,21 @@ class SchedulesController < ApplicationController
     @category_array = []
     @percent_array = []
     @color_array = []
-    @tasks.each do |t|
-      if @category_array.include?(t.category_name)
-        index_num = @category_array.index(t.category_name)
-        @percent_array.insert(index_num, (@percent_array[index_num] + t.percent))
-        @percent_array.delete_at(index_num + 1)
-      else
-        @category_array.push(t.category_name)
-        @percent_array.push(t.percent)
-        if @category_array.include?("Untitled")
-          @color_array.push("rgba(166,166,166,0.5)")
-        else
-          @color_array.push(t.task_categories[0].category.color_code)
-        end
-      end
-    end
+    add_task_analyses(@tasks)
     gon.category_array = @category_array
     gon.percent_array = @percent_array
     gon.color_array = @color_array
-    # schedule_baseロジック
-    @schedule_basis = if @schedule.pattren.include?("weekday")
-                        current_user.schedule_bases.find_by(pattren: "weekday")
-                      else
-                        current_user.schedule_bases.find_by(pattren: "holiday")
-                      end
+    @schedule_basis = pattren_select(@schedule)
     @task_bases = @schedule_basis.task_bases.order(time_start: :asc)
     @task_basis = @schedule_basis.task_bases.new
     @basis_category_array = []
     @basis_percent_array = []
     @basis_color_array = []
-    @task_bases.each do |t|
-      if @basis_category_array.include?(t.category_name)
-        index_num = @basis_category_array.index(t.category_name)
-        @basis_percent_array.insert(index_num, (@basis_percent_array[index_num] + t.percent))
-        @basis_percent_array.delete_at(index_num + 1)
-      else
-        @basis_category_array.push(t.category_name)
-        @basis_percent_array.push(t.percent)
-        if @basis_category_array.include?("Untitled")
-          @basis_color_array.push("rgba(166,166,166,0.5)")
-        else
-          @basis_color_array.push(t.category_task_bases[0].category.color_code)
-        end
-      end
-    end
+    add_task_basis_analyses(@task_bases)
     gon.basis_category_array = @basis_category_array
     gon.basis_percent_array = @basis_percent_array
     gon.basis_color_array = @basis_color_array
-    # 数値結果算出のロジック
-    @name_results = []
-    @color_results = []
-    @percent_results = []
-    task_category_results = []
-    task_category_names = @tasks.pluck(:category_name).uniq
-    #=> ["勉強", "家事", "振り返り"]
-    task_basis_category_names = @task_bases.pluck(:category_name).uniq
-    #=> ["勉強", "家事"]
-    task_category_results = task_category_names & task_basis_category_names
-    task_count = task_category_results.count
-    #=> 3
-    index_num = 0
-
-    task_count.times do
-      task_categories_sames = @tasks.where(category_name: task_category_results[index_num])
-      task_percent_sum = task_categories_sames.pluck(:percent).sum
-      task_basis_categories_sames = @task_bases.where(category_name: task_category_results[index_num])
-      task_basis_percent_sum = task_basis_categories_sames.pluck(:percent).sum
-      percent_result = if task_percent_sum > task_basis_percent_sum
-                         task_percent_sum - task_basis_percent_sum
-                       else
-                         task_basis_percent_sum - task_percent_sum
-                       end
-      @name_results.push(task_categories_sames[0].category_name)
-      @color_results.push(task_categories_sames[0].categories[0].color_code)
-      @percent_results.push(percent_result)
-      index_num += 1
-    end
+    analysis_feedback(@tasks, @task_bases)
     gon.name_results = @name_results
     gon.color_results = @color_results
     gon.percent_results = @percent_results
